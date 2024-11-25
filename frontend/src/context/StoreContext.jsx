7899fc76845c9f3bc12f7c15fec75fc1f6884ca1
Leanur Rahman <ramanleanur@gmail.com>
2024-11-25 15:11:42 +0600

    New update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,16 +1,36 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
+   const [cartItems, setCartItems] = useState({});
 
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({...prev,[itemId]:1}))
+    }
+    else {
+      setCartItems((prev)=>({...prev, [itemId]: prev[itemId]+1}))
+    }
+  }
 
-
-
+   const removeFromCart = (itemId) => {
+     setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1 }))
+   }
+  
+   useEffect(() => {
+     console.log(cartItems);
+   }, [cartItems]);
+   
+  
   const contextValue = {
-    food_list
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
   }
   
   return (
@@ -21,3 +41,87 @@ const StoreContextProvider = (props) => {
   )
 }
 export default StoreContextProvider;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createContext, useEffect, useState } from 'react';
+// import { food_list } from '../assets/assets';
+
+// export const StoreContext = createContext(null);
+
+// const StoreContextProvider = props => {
+//   const [cartItems, setCartItems] = useState({});
+
+//   const addToCart = itemId => {
+//     setCartItems(prev => ({
+//       ...prev,
+//       [itemId]: prev[itemId] ? prev[itemId] + 1 : 1,
+//     }));
+//   };
+
+//   const removeFromCart = itemId => {
+//     setCartItems(prev => {
+//       if (prev[itemId] > 1) {
+//         return { ...prev, [itemId]: prev[itemId] - 1 };
+//       } else {
+//         const { [itemId]: _, ...rest } = prev; // Remove item if count is 0
+//         return rest;
+//       }
+//     });
+//   };
+
+//   useEffect(() => {
+//     localStorage.setItem('cartItems', JSON.stringify(cartItems));
+//   }, [cartItems]);
+
+//   const contextValue = {
+//     food_list,
+//     cartItems,
+//     addToCart,
+//     removeFromCart,
+//   };
+
+//   return (
+//     <StoreContext.Provider value={contextValue}>
+//       {props.children}
+//     </StoreContext.Provider>
+//   );
+// };
+
+// export default StoreContextProvider;
